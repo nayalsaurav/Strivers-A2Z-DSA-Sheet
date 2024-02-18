9a6a785280f484f86f18d07f8d1f0989708afbc5
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-18 19:15:58 +0530

    Create check prime.cpp

---
+++ Step 1.4 : Know Basic Maths/check prime.cpp
@@ -0,0 +1,14 @@
+bool isPrime(int n)
+{
+	// Write your code here.
+	if(n==0||n==1){
+		return false;
+	}
+
+	for(int i=2;i<=sqrt(n);i++){
+		if(n%i==0){
+			return false;
+		}
+	}
+	return true;
+}
