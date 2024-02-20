8a6d1f779dc565d363e8266c22070c95597f7d06
Saurav Nayal <140402949+nayalsaurav@users.noreply.github.com>
2024-02-20 22:30:05 +0530

    Count Frequency in a range.cpp

---
+++ Step 1.6 : Learn Basic Hashing/Count Frequency in a range.cpp
@@ -0,0 +1,18 @@
+vector<int> countFrequency(int n, int x, vector<int> &nums)
+{
+    // Create a vector 'ans' of size n initialized with zeros to store the frequencies.
+    vector<int> ans(n, 0);
+
+    // Traverse the input array 'nums'.
+    for (int num : nums)
+    {
+        // Check if the element 'num' is within the range of 1 to n.
+        if (num <= n)
+        {
+            // Increment the frequency of 'num' in 'ans'.
+            ans[num - 1]++;
+        }
+    }
+    // Return the vector 'ans' containing the frequencies.
+    return ans;
+}
